3aba67c66453e21043d06e6808a0dd49f438ed24
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-11-23 23:02:30 +0200

    add header and footer

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Main.scss';
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
 
+new AirDatepicker('#calendar');
 
 const Main = () => {
   return (
-    <main className='main'>MAIN</main>
+    <main className='main'>
+      <h1 className='main-title'>Замовити</h1>
+      <form action="" className='form'>
+        <div className='form-points'>
+          <h2 className='form-title'>Маршрут</h2>
+          <div className='form-points-point'>
+            <h3 className='form-points-point-title'>Точка A</h3>
+            <div className='form-points-point-data'>
+              <div className='form-points-point-data-info'>
+                <p className='form-points-point-data-info-title'>Дата</p>
+                <div className='form-points-point-data-info-input'>
+                  <input type="text" id='calendar' />
+                </div>
+              </div>
+              <div className='form-points-point-data-info'>
+                <p className='form-points-point-data-info-title'>Час роботи (год)</p>
+                <div className='form-points-point-data-info-input'></div>
+              </div>
+              <div className='form-points-point-data-info'>
+                <p className='form-points-point-data-info-title'>Адреса</p>
+                <div className='form-points-point-data-info-input'></div>
+              </div>
+              <div className='form-points-point-data-info'>
+                <p className='form-points-point-data-info-title'>Час прибуття</p>
+                <div className='form-points-point-data-info-input'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='form-map'></div>
+      </form>
+    </main>
   )
 }
 
