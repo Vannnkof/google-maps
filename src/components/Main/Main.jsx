73499159362716d482145fcc4b07dd069a7b3a1e
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-11-27 16:35:53 +0200

    add calendar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Datepicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.module.css';
 import './Main.scss';
-import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
-
-new AirDatepicker('#calendar');
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 const Main = () => {
+  const [selectedDate, setDate] = useState(null);
+  const [value, setValue] = useState(null);
+
   return (
     <main className='main'>
       <h1 className='main-title'>Замовити</h1>
@@ -15,23 +17,44 @@ const Main = () => {
           <div className='form-points-point'>
             <h3 className='form-points-point-title'>Точка A</h3>
             <div className='form-points-point-data'>
-              <div className='form-points-point-data-info'>
-                <p className='form-points-point-data-info-title'>Дата</p>
-                <div className='form-points-point-data-info-input'>
-                  <input type="text" id='calendar' />
+              <div className='form-points-point-data-wrapper'>
+                <div className='form-points-point-data-info'>
+                  <p className='form-points-point-data-info-title'>Дата</p>
+                  <div className='form-points-point-data-info-input'>
+                    <label>
+                      <div>
+                        <img className='calendar-img' src="/Icons/calendar.svg" alt="calendar" />
+                        <Datepicker type="text" className='text-info' selected={selectedDate} onChange={date => setDate(date)} dateFormat="d.M.yy" />
+                      </div>
+                    </label>
+                  </div>
+                </div>
+                <div className='form-points-point-data-info'>
+                  <p className='form-points-point-data-info-title'>Адреса</p>
+                  <div className='form-points-point-data-info-input'>
+                    <GooglePlacesAutocomplete
+                      apiKey={process.env.NEXT_PUBLIC_API_KEY}
+                      selectProps={{
+                        value,
+                        onChange: setValue,
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className='form-points-point-data-info'>
-                <p className='form-points-point-data-info-title'>Час роботи (год)</p>
-                <div className='form-points-point-data-info-input'></div>
-              </div>
-              <div className='form-points-point-data-info'>
-                <p className='form-points-point-data-info-title'>Адреса</p>
-                <div className='form-points-point-data-info-input'></div>
-              </div>
-              <div className='form-points-point-data-info'>
-                <p className='form-points-point-data-info-title'>Час прибуття</p>
-                <div className='form-points-point-data-info-input'></div>
+              <div className='form-points-point-data-wrapper'>
+                <div className='form-points-point-data-info'>
+                  <p className='form-points-point-data-info-title'>Час роботи (год)</p>
+                  <div className='form-points-point-data-info-input'>
+                    <input className='text-info text-info-workTime' type="text" />
+                  </div>
+                </div>
+                <div className='form-points-point-data-info'>
+                  <p className='form-points-point-data-info-title'>Час прибуття</p>
+                  <div className='form-points-point-data-info-input'>
+                    <input className='text-info text-info-arriveTime' type="text" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
